3727c4a8bb3b304354d20b447ab82d0413942db5
francois goddefroy <francoisgoddefroy62@gmail.com>
2022-05-04 10:06:58 +0200

    DI + factorisation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,42 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
+    private $repoArticle;
+
+    public function __construct(ArticleRepository $repoArticle)
+    {
+        $this->repoArticle = $repoArticle;
+    }
+
     /**
-     * @Route("/home", name="app_home")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
+        $articles = $this->repoArticle->findAll();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            "articles" => $articles
+        ]);
+    }
+
+    /**
+     * @Route("/show/{id}", name="show")
+     */
+    public function show(Article $article): Response
+    {
+        if (!$article) {
+            $this->redirectToRoute("home");
+        }
+        return $this->render(' home/show.html.twig', [
+            "article" => $article
         ]);
     }
 }
