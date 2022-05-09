63ab5f861416adb2a1583efb82c3a1270671776a
francois goddefroy <francoisgoddefroy62@gmail.com>
2022-05-09 10:14:55 +0200

    les formulaires +  validation

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
@@ -20,22 +21,45 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("/register", name="security_register")
-     */
-    public function resgister(Request $request): Response
+         * @Route("/register/{user_type}", name="security_register")
+         */
+    public function resgister(Request $request, UserPasswordEncoderInterface $encoder, $user_type): Response
     {
         $user = new User();
+
+        if ($user_type === "admin" && $this->isGranted('ROLE_SUPER_ADMIN')) {
+            $user->setRoles(['ROLE_ADMIN']);
+        }
         $form = $this->createForm(RegisterType::class, $user);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            $password_hash = $passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($password_hash);
+
             $this->manager->persist($user);
             $this->manager->flush();
-            return $this->redirectToRoute("home");
+            return $this->redirectToRoute("security_login");
         }
+
         return $this->render('security/index.html.twig', [
             'controller_name' => "Inscription",
             'form' => $form->createView(),
         ]);
+    }
+    /**
+         * @Route("/login", name="security_login")
+         */
+    public function login(): Response
+    {
+        return $this->render('security/login.html.twig', [
+
+        ]);
+    }
+    /**
+         * @Route("/logout", name="security_logout")
+         */
+    public function logout()
+    {
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,15 +2,21 @@
 
 namespace App\Entity;
 
+use App\Entity\Article;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ORM\Table(name="`user`")
+ * @UniqueEntity(fields="username", message="Le nom d'utilisateur existe déjà.")
+ * @UniqueEntity(fields="email", message="L'adresse mail existe déjà.")
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
@@ -34,24 +40,50 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @var string The hashed password
      * @ORM\Column(type="string")
-     */
+     * @Assert\Regex(
+     *     pattern="/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/",
+     *     message="Le mot de passe est dans un format invalide."
+     * )
+     * @Assert\NotCompromisedPassword(
+     *      message="Le mot de passe est compromis, veuillez le changer ."
+     * )
+     */
     private $password;
-
+    
+    /**
+     * @var string
+     * passwordConfirm (juste pour vérifier - pas en bdd)
+     * @Assert\EqualTo(
+     *      propertyPath = "password", 
+     *      message="les deux mots de passes ne sont pas identiques"
+     * )
+     */
     private $passwordConfirm;
 
     /**
-     * @ORM\Column(type="string", length=255)
-     */
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Regex(
+     *     pattern="/^[\p{L}\s]{2,}$/u",
+     *     message="Le prénom ne doit contenir que des lettres, 2 caractères minmum."
+     * )
+     */
     private $firstName;
 
     /**
-     * @ORM\Column(type="string", length=255)
-     */
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Regex(
+     *     pattern="/^[\p{L}\s]{2,}$/u",
+     *     message="Le nom de famille ne doit contenir que des lettres, 2 caractères minmum."
+     * )
+     */
     private $lastName;
 
     /**
-     * @ORM\Column(type="string", length=255)
-     */
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Email(
+     *     message = "L'adresse mail n'est pas valide."
+     * )
+     */
     private $email;
 
     /**
